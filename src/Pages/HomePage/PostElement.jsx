b5ac9108afac97b11f0734754895d6bb2aa4698d
baboshi <abdelhameid@abdelhameid>
2024-01-20 21:20:24 +0200

    some css changes

--- src/Pages/HomePage/PostElement.jsx
+++ src/Pages/HomePage/PostElement.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 export const PostElement = (props) => {
-    const { text, createdAt, userName,titleValue } = props.post;
-    console.log(titleValue)
+    const { text, createdAt, userName, titleValue } = props.post;
+    console.log(titleValue);
     const date = createdAt?.toDate().toLocaleDateString();
     const time = createdAt?.toDate().toLocaleTimeString();
     return (
-        <div className="bg-slate-400 p-8 w-1/2 relative text-blue-900">
-            <div className="absolute right-1 bottom-0">
-                {date + "  " + time}
+        <div className="bg-slate-400 px-2 py-2 w-1/2 text-left relative  text-blue-900">
+            <div className="mb-2">
+                <small className="inline text-black ">u/{userName}</small>
+                <small className="inline text-slate-100">{"     "+ date + "  " + time}</small>
             </div>
-            <div>
-                Title: {titleValue}
-            </div>
+            <h1 className="text-black text-2xl
+            ">{titleValue}</h1>
+            <div className="p-6  ">
             <h1>{text}</h1>
 
-            <div className="absolute left-1 top-0">r/{userName}</div>
+            </div>
         </div>
     );
 };
