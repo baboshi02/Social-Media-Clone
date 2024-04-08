aa8c6ccb2c6322faeffc1f16ccebe0e31c02e831
baboshi <abdelhameid@abdelhameid>
2024-04-09 01:09:56 +0200

    added enviromental variables

--- src/context/PostsContext.jsx
+++ src/context/PostsContext.jsx
@@ -7,31 +7,27 @@ import { useCollection, useDocumentData } from "react-firebase-hooks/firestore";
 export const PostContext = createContext();
 
 const getPosts = () => {
-    let posts = [];
-    const postsRef = collection(db, "Posts");
-    const [snapShot, loading,error] = useCollection(postsRef, { idField: "id" });
-    console.log("error: " + error)
-    snapShot?.forEach((doc) => {
-        posts.push({ docID: doc.id, docData: doc.data() });
-    });
-    return [posts, loading];
+  let posts = [];
+  const postsRef = collection(db, "Posts");
+  const [snapShot, loading, error] = useCollection(postsRef, { idField: "id" });
+  snapShot?.forEach((doc) => {
+    posts.push({ docID: doc.id, docData: doc.data() });
+  });
+  return [posts, loading];
 };
 
 const getUsers = () => {
-    const { uid } = auth.currentUser;
-    const userNameRef = doc(db, "users", uid);
-    const [users, ,error] = useDocumentData(userNameRef);
-    console.log("error: ", error)
-    return users;
+  const { uid } = auth.currentUser;
+  const userNameRef = doc(db, "users", uid);
+  const [users, , error] = useDocumentData(userNameRef);
+  return users;
 };
 
 export const PostContextProvider = (props) => {
-    const [posts, loading] = getPosts();
-    const users = getUsers();
-    const values = { posts, loading, users };
-    return (
-        <PostContext.Provider value={values}>
-            {props.children}
-        </PostContext.Provider>
-    );
+  const [posts, loading] = getPosts();
+  const users = getUsers();
+  const values = { posts, loading, users };
+  return (
+    <PostContext.Provider value={values}>{props.children}</PostContext.Provider>
+  );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-import {getAuth} from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,13 +9,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVr2upjKek-dnITSg_WkOvHEyPw9_NEbg",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "react-clone-fb01c.firebaseapp.com",
   projectId: "react-clone-fb01c",
   storageBucket: "react-clone-fb01c.appspot.com",
   messagingSenderId: "603138987529",
   appId: "1:603138987529:web:b188e919fe3f9173250ad8",
-  measurementId: "G-79FDS0WD4M"
+  measurementId: "G-79FDS0WD4M",
 };
 
 // Initialize Firebase
